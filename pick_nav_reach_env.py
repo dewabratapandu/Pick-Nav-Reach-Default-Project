695e05ae7cf478c20ff5919f3d3ec5716d448924
Pandu Dewabrata <dewabrata.panda@gmail.com>
2025-11-26 13:04:50 +0800

    Antipodal: NOT FIX

--- pick_nav_reach_env.py
+++ pick_nav_reach_env.py
@@ -7,6 +7,8 @@ import trimesh
 from pathlib import Path
 from maze_utils import generate_maze_map, add_left_room_to_maze, create_maze_urdf
 from copy import deepcopy
+
+from src.antipodal import AntiPodalGrasping
 from keyboard_control import KeyBoardController
 from utils import closest_joint_values
 
@@ -65,17 +67,18 @@ class PickNavReachEnv:
             base_ori,
             useFixedBase=True,
             # Pandu's Note: in the original code, flags was commented. But somehow commenting it made the robot shake uncontrollably
-            flags=p.URDF_USE_SELF_COLLISION | p.URDF_USE_SELF_COLLISION_EXCLUDE_ALL_PARENTS,
+            # flags=p.URDF_USE_SELF_COLLISION | p.URDF_USE_SELF_COLLISION_EXCLUDE_ALL_PARENTS,
         )
 
         # Collect joint info (skip fixed)
         n_joints = p.getNumJoints(self.robot_id)
-        indices = []
+        indices, link_names = [], []
         lowers, uppers, ranges, rest = [], [], [], []
 
         for j in range(n_joints):
             info = p.getJointInfo(self.robot_id, j)
             joint_type = info[2]
+            link_names.append(info[12].decode("utf-8"))
             if joint_type in (p.JOINT_REVOLUTE, p.JOINT_PRISMATIC):
                 indices.append(j)
                 lowers.append(info[8])
@@ -109,6 +112,16 @@ class PickNavReachEnv:
             self.joint_upper,
         )
         self._set_qpos(self.init_qpos)
+        
+        # Get the link id for the hand (EEF)
+        eef_link_name = "bh_base_link" if self.use_barret_hand else "gripper_link"
+        self.eef_id = link_names.index(eef_link_name)
+        self.eef_marker = p.loadURDF("assets/frame_marker/frame_marker_target.urdf", [0,0,0], useFixedBase=True, globalScaling=0.25)
+        print(f"End Effector Link ID: {self.eef_id}")
+        
+        # To make the fingers "sticky"
+        # p.changeDynamics(self.robot_id, 13, lateralFriction=2.0)
+        # p.changeDynamics(self.robot_id, 14, lateralFriction=2.0)
 
     def _load_object_table(self):
         p.setAdditionalSearchPath(pybullet_data.getDataPath()) 
@@ -269,7 +282,7 @@ class PickNavReachEnv:
             controlMode=p.POSITION_CONTROL,
             # controlMode=p.PD_CONTROL,
             targetPositions=target.tolist(),
-            # targetVelocities=[0.0] * len(self.joint_indices),
+            targetVelocities=[0.3] * len(self.joint_indices),
             forces=[self.max_force] * len(self.joint_indices),
             # positionGains=[0.3] * len(self.joint_indices),
             positionGains=position_gains,
@@ -325,14 +338,14 @@ class PickNavReachEnv:
 
         # self.visualize_pc_and_normals(object_pc, object_normals, visualize_normals=False)
         return {
-            "qpos": deepcopy(qpos),
-            "qvel": deepcopy(qvel),
-            "object_pos": deepcopy(object_pos),
-            "object_xyzw": deepcopy(object_xyzw),
-            "goal_pos": deepcopy(self.goal_pos),
-            "object_pc": deepcopy(object_obs["object_pc"]),
-            "object_normals": deepcopy(object_obs["object_normals"]),
-            "cube_positions": deepcopy(self.cube_positions[:, :2]),
+            "qpos": deepcopy(qpos), # Robot joint position state (angle), (num_state,)
+            "qvel": deepcopy(qvel), # Robot joint velocity state, (num_state,)
+            "object_pos": deepcopy(object_pos), # Center of object position, (x, y, z)
+            "object_xyzw": deepcopy(object_xyzw), # Orientation of the object, (x, y, z, w)
+            "object_pc": deepcopy(object_obs["object_pc"]), # Point Cloud of the object, (1024, 3)
+            "object_normals": deepcopy(object_obs["object_normals"]), # Where the point cloud facing, (1024, 3)
+            "cube_positions": deepcopy(self.cube_positions[:, :2]), # For navigation through the labyrinth
+            "goal_pos": deepcopy(self.goal_pos), # Goal position
         }
 
     def _get_state(self):
@@ -370,9 +383,13 @@ class PickNavReachEnv:
         qpos, _, _, _ = self._get_state()
         return qpos.copy()
 
+    def is_object_ready(self):
+        obs = self._get_obs()
+        is_ready = not np.any(obs["object_pos"] == 0)
+        return obs, is_ready
 
 if __name__ == "__main__":
-    USE_BARRET_HAND = True
+    USE_BARRET_HAND = False
     
     # It will load the robot and the environment
     # Since we also want to modify the robot, we should change this one too.
@@ -383,16 +400,25 @@ if __name__ == "__main__":
     # This is the main part we should replace.
     # There are 15 units of action we should control. Check keyboard-action-readme.md!
     keyboard_controller = KeyBoardController(env, use_barret_hand=USE_BARRET_HAND)
+    antipodal_controller = AntiPodalGrasping(env.robot_id, range(6,13), [13,14])
     
     # for i in range (10000):
     import time
     while True:
+        obs, is_ready = env.is_object_ready()
+        
         # random_action = np.random.uniform(-1.0, 1.0, size=(env.action_size,))
-        action = keyboard_controller.get_action()
+        # action = keyboard_controller.get_action()
+        action = antipodal_controller.get_action(obs)
         obs, info = env.step(action)
         for k, v in info.items():
             print(f"{k}: {v}")
         # p.stepSimulation()
         # time.sleep(1./240.)
 
-
+        # Visualize current EEF frame
+        # eef_marker = p.loadURDF("assets/frame_marker/frame_marker_target.urdf", [0,0,0],
+        #                 useFixedBase=True, globalScaling=0.25)
+        # eef_state = p.getLinkState(env.robot_id, env.eef_id)
+        # curr_pos, curr_orn = eef_state[4], eef_state[5]
+        # p.resetBasePositionAndOrientation(eef_marker, curr_pos, curr_orn)
